c53d4a665b638a8cd25ec33ff37bf287e4ed5e06
craft war <bay.asanov@mail.ru>
2025-01-17 21:03:46 +0600

    docs: add XML documentation for IPropertiesSyntaxGenerator interface

--- PropertyBitPack.SourceGen/PropertiesSyntaxGenerators/IPropertiesSyntaxGenerator.cs
+++ PropertyBitPack.SourceGen/PropertiesSyntaxGenerators/IPropertiesSyntaxGenerator.cs
@@ -8,11 +8,23 @@ using System.Collections.Immutable;
 using System.Text;
 
 namespace PropertyBitPack.SourceGen.PropertiesSyntaxGenerators;
+
+/// <summary>
+/// Defines the contract for generating syntax for properties based on source generation requests.
+/// </summary>
+/// <remarks>
+/// Implementations of this interface are responsible for processing source generation requests
+/// and producing file generator requests, excluding aggregated properties.
+/// </remarks>
 internal interface IPropertiesSyntaxGenerator
 {
     /// <summary>
-    /// Remove properties in <paramref name="requests"/> which aggregated
+    /// Generates file generator requests based on the provided source generation requests,
+    /// excluding aggregated properties.
     /// </summary>
+    /// <param name="requests">The list of source generation requests to process.</param>
+    /// <returns>
+    /// An <see cref="ImmutableArray{T}"/> of <see cref="FileGeneratorRequest"/> objects representing the generated requests.
+    /// </returns>
     public ImmutableArray<FileGeneratorRequest> Generate(ILinkedList<GenerateSourceRequest> requests);
-
 }
