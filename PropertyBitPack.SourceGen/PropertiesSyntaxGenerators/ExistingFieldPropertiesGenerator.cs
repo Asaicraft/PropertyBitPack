f1a7533c139d18d8505c36d1d528bcde7c1b04d4
craft war <bay.asanov@mail.ru>
2025-01-17 20:37:56 +0600

    feat: add ExistingFieldPropertiesGenerator for field properties
    Introduce ExistingFieldPropertiesGenerator to generate source code for
    existing field properties. Override GenerateCore and Generate methods
    to filter types and create formatted source text. Add helper methods
    for building properties and compilation units.

---
+++ PropertyBitPack.SourceGen/PropertiesSyntaxGenerators/ExistingFieldPropertiesGenerator.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis;
+using PropertyBitPack.SourceGen.Collections;
+using PropertyBitPack.SourceGen.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace PropertyBitPack.SourceGen.PropertiesSyntaxGenerators;
+internal sealed class ExistingFieldPropertiesGenerator: BasePropertiesSyntaxGenerator
+{
+    protected override void GenerateCore(ILinkedList<GenerateSourceRequest> requests, in ImmutableArrayBuilder<FileGeneratorRequest> immutableArrayBuilder)
+    {
+        ImmutableArray<ExistingFieldGsr> candidateRequests;
+
+        using (var candidateRequestsBuilder = ImmutableArrayBuilder<ExistingFieldGsr>.Rent())
+        {
+            foreach (var candidateRequest in requests)
+            {
+                if (candidateRequest is ExistingFieldGsr existingFieldGsr)
+                {
+                    candidateRequestsBuilder.Add(existingFieldGsr);
+                    continue;
+                }
+            }
+
+            candidateRequests = candidateRequestsBuilder.ToImmutable();
+        }
+
+        for (var i = 0; i < candidateRequests.Length; i++)
+        {
+            var candidateRequest = candidateRequests[i];
+
+            var sourceText = Generate(candidateRequest);
+            var fileName = GetFileName(candidateRequest);
+
+            requests.Remove(candidateRequest);
+
+            immutableArrayBuilder.Add(new FileGeneratorRequest(sourceText, fileName));
+        }
+
+    }
+
+    private SourceText Generate(ExistingFieldGsr nonExistingFieldGsr)
+    {
+        using var propertiesRented = ListsPool.Rent<PropertyDeclarationSyntax>();
+        var properties = propertiesRented.List;
+
+        using var additionalMembersRented = ListsPool.Rent<MemberDeclarationSyntax>();
+        var additionalMembersList = additionalMembersRented.List;
+
+        for (var i = 0; i < nonExistingFieldGsr.Properties.Length; i++)
+        {
+            var propertyRequest = nonExistingFieldGsr.Properties[i];
+
+            properties.Add(GenerateProperty(nonExistingFieldGsr, propertyRequest, out var additionalMembers));
+
+            if (!additionalMembers.IsDefaultOrEmpty)
+            {
+                additionalMembersList.AddRange(additionalMembers);
+            }
+        }
+
+        using var membersRented = ListsPool.Rent<MemberDeclarationSyntax>();
+        var members = membersRented.List;
+
+        members.AddRange(properties);
+        members.AddRange(additionalMembersList);
+
+        var unit = GenerateCompilationUnit(nonExistingFieldGsr, members);
+
+        return unit.NormalizeWhitespace().GetText(Encoding.UTF8);
+    }
+}
