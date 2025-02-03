370817963aee33d276abadace7966fa0f8300467
craft war <bay.asanov@mail.ru>
2025-02-03 22:07:06 +0600

    Refactor aggregators and add dictionary pool
    Updated ReadOnlyAggregator to use System.Collections.Immutable and inherit from BaseUnnamedFieldAggregator. Removed AggregateCore method. Modified PropertyBitPack.SourceGen.csproj to remove compile reference to IBitsMappingAttributeBase.cs. Added DictionariesSymbolPool{TValue}.cs to manage a pool of dictionaries with ISymbol keys and TValue values using ConcurrentStack.

--- PropertyBitPack.SourceGen/BitFieldPropertyAggregators/ReadOnlyAggregator.cs
+++ PropertyBitPack.SourceGen/BitFieldPropertyAggregators/ReadOnlyAggregator.cs
@@ -5,6 +5,7 @@ using PropertyBitPack.SourceGen.Models.AttributeParsedResults;
 using PropertyBitPack.SourceGen.Models.GenerateSourceRequests;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Text;
 
 namespace PropertyBitPack.SourceGen.BitFieldPropertyAggregators;
@@ -16,47 +17,7 @@ namespace PropertyBitPack.SourceGen.BitFieldPropertyAggregators;
 /// This class inherits from <see cref="BaseBitFieldPropertyAggregator"/> and provides functionality 
 /// to filter and aggregate properties marked as read-only for further processing.
 /// </remarks>
-internal sealed class ReadOnlyAggregator: BaseBitFieldPropertyAggregator
+internal sealed class ReadOnlyAggregator : BaseUnnamedFieldAggregator
 {
 
-    /// <summary>
-    /// Aggregates properties with the <see cref="ParsedReadOnlyBitFieldAttribute"/> into an immutable array.
-    /// </summary>
-    /// <param name="properties">
-    /// A linked list of <see cref="BaseBitFieldPropertyInfo"/> representing the properties to be aggregated.
-    /// </param>
-    /// <param name="requestsBuilder">
-    /// An <see cref="ImmutableArrayBuilder{T}"/> used to accumulate generation requests.
-    /// </param>
-    /// <param name="diagnostics">
-    /// An <see cref="ImmutableArrayBuilder{T}"/> used to collect diagnostics during the aggregation process.
-    /// </param>
-    /// <remarks>
-    /// This method filters properties that have a parsed result of type <see cref="ParsedReadOnlyBitFieldAttribute"/>
-    /// and collects them into a temporary builder, which is then converted into an immutable array for further use.
-    /// </remarks>
-    protected override void AggregateCore(ILinkedList<BaseBitFieldPropertyInfo> properties, in ImmutableArrayBuilder<GenerateSourceRequest> requestsBuilder, in ImmutableArrayBuilder<Diagnostic> diagnostics)
-    {
-        // We'll gather properties that do not define a valid field name into a temporary builder
-        using var readonlyFieldPropertiesBuilder = ImmutableArrayBuilder<BaseBitFieldPropertyInfo>.Rent();
-
-        foreach (var property in properties)
-        {
-            if (property.AttributeParsedResult is IParsedReadOnlyBitFieldAttribute)
-            {
-                readonlyFieldPropertiesBuilder.Add(property);
-            }
-        }
-
-        var readonlyFieldProperties = readonlyFieldPropertiesBuilder.ToImmutable();
-
-        if(readonlyFieldProperties.IsDefaultOrEmpty)
-        {
-            return;
-        }
-
-        var grouped = GroupPropertiesByFieldNameAndOwner(readonlyFieldProperties);
-
-
-    }
 }
---
+++ PropertyBitPack.SourceGen/Collections/DictionariesSymbolPool{TValue}.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PropertyBitPack.SourceGen.Collections;
+internal static class DictionariesSymbolPool<TValue>
+{
+    private static readonly ConcurrentStack<Dictionary<ISymbol, TValue>> pool = [];
+
+    static DictionariesSymbolPool()
+    {
+        pool.Push(new(SymbolEqualityComparer.Default));
+        pool.Push(new(SymbolEqualityComparer.Default));
+    }
+
+    public static Dictionary<ISymbol, TValue> Rent()
+    {
+        if (pool.TryPop(out var dictionary))
+        {
+            return dictionary;
+        }
+        return new(SymbolEqualityComparer.Default);
+    }
+
+    public static void Return(Dictionary<ISymbol, TValue> dictionary)
+    {
+        if (pool.Count > 4)
+        {
+            return;
+        }
+
+        if(dictionary.Comparer != SymbolEqualityComparer.Default)
+        {
+            return;
+        }
+
+        dictionary.Clear();
+        pool.Push(dictionary);
+    }
+
+    public static RentedToken RentToken()
+    {
+        return new(Rent());
+    }
+
+    public readonly ref struct RentedToken
+    {
+        private readonly Dictionary<ISymbol, TValue> _dictionary;
+        public RentedToken(Dictionary<ISymbol, TValue> dictionary)
+        {
+            _dictionary = dictionary;
+        }
+
+        public void Dispose()
+        {
+            DictionariesSymbolPool<TValue>.Return(_dictionary);
+        }
+    }
+}
