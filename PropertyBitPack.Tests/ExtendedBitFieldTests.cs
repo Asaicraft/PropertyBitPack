cae97f43ff2034d9be79541732cd2421d997135a
craft war <bay.asanov@mail.ru>
2025-02-24 04:30:37 +0600

    test: add ExtendedBitFieldTests with new test methods
    Introduced `ExtendedBitFieldTests` to validate extended bit-field behavior.
    
    ### Key changes:
    - Created `ExtendedBitFieldTests` in `PropertyBitPack.Tests` namespace.
    - Added `TestExtendedBitField` to verify property settings.
    - Implemented `VerifyExtendedBitFieldInternalField` to check internal field.

---
+++ PropertyBitPack.Tests/ExtendedBitFieldTests.cs
@@ -0,0 +1,45 @@
+﻿using PropertyBitPack.Example;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyBitPack.Tests;
+
+public class ExtendedBitFieldTests
+{
+    [Fact]
+    public void TestExtendedBitField()
+    {
+        var extendedBitField = new ExtendedBitFieldExample
+        {
+            SlotCount = 156, // 11 bits max value is 2047
+            SlotCount2 = 981, // 11 bits max value is 2047
+            SlotCount3 = 15683, // The resulting value will be calculated as: 123 * 6 + 2^11 = 123 * 6 + 2048 = 738 + 2048 = 2786. Because the value is greater than 2047, we will use extended value 12345
+            SlotCount4 = 2012, // 11 bits max value is 2047
+            SlotCount5 = 313 // 11 bits max value is 2047
+        };
+
+        Assert.Equal(156, extendedBitField.SlotCount);
+        Assert.Equal(981, extendedBitField.SlotCount2);
+        Assert.Equal(2786, extendedBitField.SlotCount3);
+        Assert.Equal(2012, extendedBitField.SlotCount4);
+        Assert.Equal(313, extendedBitField.SlotCount5);
+    }
+
+    [Fact]
+    public void VerifyExtendedBitFieldInternalField()
+    {
+        var extendedBitField = typeof(ExtendedBitFieldExample);
+
+        var fields = extendedBitField.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+        Assert.Single(fields);
+
+        var field = fields[0];
+
+        Assert.Equal(typeof(ulong), field.FieldType);
+    }
+}
