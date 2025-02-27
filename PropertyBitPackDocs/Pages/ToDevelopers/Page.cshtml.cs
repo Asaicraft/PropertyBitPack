4e9dc6a9a4f78909afe5fe7c30cd18db20525599
craft war <bay.asanov@mail.ru>
2025-02-27 11:59:29 +0600

    docs: clean up and restructure documentation pages
    Removed content from `Index.cshtml` and `Page.cshtml`. Deleted `WhatsNew.cshtml`. Introduced `to_developers` route and updated models, injections, and partial views.

---
+++ PropertyBitPackDocs/Pages/ToDevelopers/Page.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PropertyBitPackDocs.Pages.ToDevelopers;
+
+public class Page : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
