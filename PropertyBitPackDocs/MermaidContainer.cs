d8c664822e7987bf4019c7054be37d2da4308dc9
craft war <bay.asanov@mail.ru>
2025-02-27 14:44:26 +0600

    refactor: update MermaidContainer to use GetRawText method
    - Added `GetRawText` method to recursively extract raw text from `Block` objects.
    - Modified `Write` method to use `GetRawText` for extracting content.
    - Ensured content is written inside `<pre class="mermaid">` and `<code class="language-mermaid">` tags.
    - Defaulted `summary` variable to `"source"` when `obj.Arguments` is `null`.

--- PropertyBitPackDocs/MermaidContainer.cs
+++ PropertyBitPackDocs/MermaidContainer.cs
@@ -1,23 +1,71 @@
 ﻿using Markdig.Extensions.CustomContainers;
 using Markdig.Renderers;
 using Markdig.Renderers.Html;
+using Markdig.Syntax;
+using System.Text;
 
 namespace PropertyBitPackDocs;
 
 public class MermaidContainer : HtmlObjectRenderer<CustomContainer>
 {
+    public static string GetRawText(Block block)
+    {
+        // Если это листовой блок, то собираем строки
+        if (block is LeafBlock leafBlock)
+        {
+            // Если у блока нет строк, вернём пустую строку
+            if (leafBlock.Lines.Lines == null)
+            {
+                return string.Empty;
+            }
+
+            // Превращаем коллекцию StringLine в обычный текст
+            var lines = leafBlock.Lines.Lines;
+            var lineStrings = new string[lines.Length];
+            for (var i = 0; i < lines.Length; i++)
+            {
+                lineStrings[i] = lines[i].ToString();
+            }
+            return string.Join("\n", lineStrings);
+        }
+        // Если это контейнерный блок, обходим всех детей рекурсивно
+        else if (block is ContainerBlock container)
+        {
+            var sb = new StringBuilder();
+            foreach (var child in container)
+            {
+                var childRaw = GetRawText(child);
+                if (!string.IsNullOrEmpty(childRaw))
+                {
+                    // Чтобы между блоками была хотя бы новая строка
+                    if (sb.Length > 0)
+                    {
+                        sb.AppendLine();
+                    }
+
+                    sb.Append(childRaw);
+                }
+            }
+            return sb.ToString();
+        }
+        // На всякий случай, если это какой-то неизвестный тип блока
+        return string.Empty;
+    }
+
     protected override void Write(HtmlRenderer renderer, CustomContainer obj)
     {
         renderer.EnsureLine();
 
         var attrs = obj.TryGetAttributes()!;
 
-        var summary = obj.Arguments;
+        var summary = obj.Arguments ?? "source";
+
+        var rawContent = GetRawText(obj);
 
         renderer.Write("<div").WriteAttributes(obj).Write('>');
         {
             renderer.Write("<pre class=\"mermaid\">");
-            renderer.WriteChildren(obj);
+            renderer.Write(rawContent);
             renderer.WriteLine("</pre>");
 
             renderer.EnsureLine();
@@ -32,7 +80,7 @@ public class MermaidContainer : HtmlObjectRenderer<CustomContainer>
                 {
                     renderer.Write("<code class=\"language-mermaid\">");
                     {
-                        renderer.WriteChildren(obj);
+                        renderer.Write(rawContent);
                     }
                     renderer.Write("</code>");
                 }
